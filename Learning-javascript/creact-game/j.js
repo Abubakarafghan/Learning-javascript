85b9ac8bb672102f72041ce3c99793fb701e0105
Abubakarafghan <139345105+Abubakarafghan@users.noreply.github.com>
2024-05-15 18:23:07 +0500

    game changes

--- Learning-javascript/creact-game/j.js
+++ Learning-javascript/creact-game/j.js
@@ -3,10 +3,30 @@ let compScore = 0;
 
 const Choices = document.querySelectorAll(".choice");
 
+const genCompChoice = (() => { 
+    const opstion =["rock", "paper", "scissors"]
+    const rendIdx = Math.floor(Math.random() * 3);
+    return opstion[rendIdx];
+})
+
+const drawGame = (()=> {
+    console.log("game was draw.");
+})
+
+const PlayGame = ((userChoice) => {
+    console.log("user choice", userChoice);
+    const compChoice = genCompChoice();
+    console.log("comp choice", compChoice);
+
+    if(userChoice === compChoice){
+        drawGame();
+    }
+})
+
 Choices.forEach((choice) => {
-    console.log(choice)
+    console.log(choice);
     choice.addEventListener("click", () => {
         let userChoice = choice.getAttribute("id");
-        console.log("you are", userChoice)
+        PlayGame (userChoice)
     })
 })
