b02d5f29b833f0f97cd053b030ce2c127f8f7263
Abubakarafghan <139345105+Abubakarafghan@users.noreply.github.com>
2025-10-24 01:56:08 +0500

    Update python.py

---
+++ python.py
@@ -0,0 +1,7 @@
+from termcolor import colored
+
+print(colored("😂😂 Just the colors! 😂😂", "red"))
+print(colored("Let's make it green!", "green"))
+print(colored("Or maybe blue? 😎", "blue"))
+Reply=("yeah exactly this is right 👍🏻☺️🤓")
+print (reply)
